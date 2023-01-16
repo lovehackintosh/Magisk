6b694bafffeb248ec78dcac5dedcfaf103052a55
Linhui Liu <liulinhui36@gmail.com>
2023-01-16 17:41:26 +0800

    CI: test

--- native/src/Application.mk
+++ native/src/Application.mk
@@ -4,13 +4,13 @@ APP_CFLAGS       := -Wall -Oz -fomit-frame-pointer -flto
 APP_LDFLAGS      := -flto
 APP_CPPFLAGS     := -std=c++20
 APP_STL          := none
-APP_PLATFORM     := android-21
+APP_PLATFORM     := android-30
 APP_THIN_ARCHIVE := true
 APP_STRIP_MODE   := --strip-all
 
 # Busybox should use stock libc.a
 ifdef B_BB
-APP_PLATFORM     := android-26
+APP_PLATFORM     := android-30
 APP_LDFLAGS      += -T src/lto_fix.lds
 ifeq ($(OS),Windows_NT)
 APP_SHORT_COMMANDS := true
